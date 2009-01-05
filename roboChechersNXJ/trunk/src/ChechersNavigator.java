a077287272cee4b45ade371630e3be83b1ce3aab
matteo <matteo@5050fb14-fe7b-11dd-ada4-5153b8187bf2>
2009-01-05 13:20:12 +0000

    - Implementata la prima parte di calibrate(); - Modificata la mappa di calibrazione (img/grid.psd)

--- roboChechersNXJ/trunk/src/ChechersNavigator.java
+++ roboChechersNXJ/trunk/src/ChechersNavigator.java
@@ -1,5 +1,8 @@
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
 import lejos.nxt.addon.ColorSensor;
 /**
  * Move the robot over the select chechers's cell
@@ -10,6 +13,8 @@ public class ChechersNavigator {
 	private int x;
 	private int y;
 	private int lashA, lashB;
+	private Motor MA, MB;
+	private ColorSensor CS = null;
 	
 	int[] posx={-7500, -5700, -4250, -2800, -1450, 0 , 1400, 3000};
 	int[] posy={0, 1000, 2000, 3000, 4000, 5000 , 6000, 7000};
@@ -19,17 +24,41 @@ public class ChechersNavigator {
 	
 	public static ChechersNavigator getInstance(){
 		if (navigator == null)
-			navigator = new ChechersNavigator();
+			navigator = new ChechersNavigator(Motor.A, Motor.B, new ColorSensor(SensorPort.S1));
 		return navigator;
 	}
-	private ChechersNavigator () {
-		Motor.A.resetTachoCount();
-		Motor.B.resetTachoCount();
+
+	public static ChechersNavigator getInstance(Motor MA, Motor MB){
+		if (navigator == null)
+			navigator = new ChechersNavigator(MA,MB,new ColorSensor(SensorPort.S1));
+		return navigator;
+	}
+
+	public static ChechersNavigator getInstance(Motor MA, Motor MB, SensorPort port){
+		if (navigator == null)
+			navigator = new ChechersNavigator(MA,MB,new ColorSensor(port));
+		return navigator;
+	}
+
+	
+	public static ChechersNavigator getInstance(Motor MA, Motor MB, ColorSensor CS){
+		if (navigator == null)
+			navigator = new ChechersNavigator(MA,MB,CS);
+		return navigator;
+	}
+	
+	private ChechersNavigator (Motor MA, Motor MB, ColorSensor CS) {
+		this.MA = MA;
+		this.MB = MB;
+		this.CS = CS;
+		this.MA.resetTachoCount();
+		this.MB.resetTachoCount();
 		this.x = 5;
 		this.y = 0;
 		this.lashA = 90;
 		this.lashB = 230;
 	}
+
 	public int getX() {
 		return x;
 	}
@@ -38,6 +67,14 @@ public class ChechersNavigator {
 		return y;
 	}
 
+	public Motor getMotorA () {
+		return MA;
+	}
+
+	public Motor getMotorB () {
+		return MB;
+	}
+	
 	public void goTo(int newX, int newY) {
 		int destAngleA, destAngleB;
 		
@@ -61,25 +98,25 @@ public class ChechersNavigator {
 		destAngleB = posx[newX];
 				
 		//controlla se deve recuperare il gioco
-		if (destAngleA < Motor.A.getTachoCount()) {
+		if (destAngleA < this.MA.getTachoCount()) {
 			destAngleA -= lashA;
 			LCD.drawString("gioco A", 0, 2);
 		}
 		
-		if (destAngleB < Motor.B.getTachoCount()) {
+		if (destAngleB < this.MB.getTachoCount()) {
 			destAngleB -= lashB;
 			LCD.drawString("gioco B", 0, 3);
 		}
 		LCD.refresh();	
 		
-		Motor.A.rotateTo(destAngleA,true);
-		Motor.B.rotateTo(destAngleB,true);
-		waitForMotors(new Motor[]{Motor.A, Motor.B});
+		this.MA.rotateTo(destAngleA,true);
+		this.MB.rotateTo(destAngleB,true);
+		waitForMotors(new Motor[]{this.MA, this.MB});
 		
 		LCD.drawString("A:", 0, 4);
-		LCD.drawInt(Motor.A.getTachoCount(), 4, 4);
+		LCD.drawInt(this.MA.getTachoCount(), 4, 4);
 		LCD.drawString("B:", 0, 5);
-		LCD.drawInt(Motor.B.getTachoCount(), 4, 5);
+		LCD.drawInt(this.MB.getTachoCount(), 4, 5);
 		LCD.refresh();
 		
 		this.x=newX;
@@ -87,12 +124,12 @@ public class ChechersNavigator {
 	}
 
 	public boolean isMoving() {
-		return Motor.A.isMoving() || Motor.B.isMoving();
+		return this.MA.isMoving() || this.MB.isMoving();
 	}
 
 	public void setSpeed(int speedA, int speedB) {
-		Motor.A.setSpeed(speedA);
-		Motor.B.setSpeed(speedB);
+		this.MA.setSpeed(speedA);
+		this.MB.setSpeed(speedB);
 	}
 
 	/***
@@ -102,8 +139,39 @@ public class ChechersNavigator {
 	 * @author Matteo
 	 */
 	public void calibrate(ColorSensor CS) {
-		
+		int color = -1;
+		while (!Button.ESCAPE.isPressed() && color != 9) {
+			color = (int)CS.getColorNumber();
+			switch (color) {
+				case 9: 
+					MA.stop();
+					MB.stop();
+					break;
+				case 0:
+					MA.stop();
+					MB.backward();
+					break;
+				default:
+					MA.forward();
+					MB.stop();
+			}
+			LCD.clear();
+			LCD.drawString("color", 0, 0);
+			LCD.drawInt((int)CS.getColorNumber(),7,0);
+			LCD.refresh();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {}
+		}
 	}
+	
+	public void calibrate() {
+		if (this.CS == null)
+			calibrate(new ColorSensor(SensorPort.S1));
+		else
+			calibrate(CS);
+	}
+	
 	/***
 	 * Wait for motor(s) in motorList to stop
 	 * @throws InterruptedException 
---
+++ roboChechersNXJ/trunk/src/calibrateTest.java
@@ -0,0 +1,9 @@
+
+public class calibrateTest {
+
+	public static void main(String[] args) throws Exception {
+		ChechersNavigator navigator= ChechersNavigator.getInstance();
+		navigator.calibrate();
+	}
+
+}
