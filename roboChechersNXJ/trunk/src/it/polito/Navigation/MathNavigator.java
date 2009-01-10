e9d27136437d0a2c207e2a310ce6365a8cdc4005
matteo <matteo@5050fb14-fe7b-11dd-ada4-5153b8187bf2>
2009-01-10 13:29:02 +0000

    aggiunto il nuovo Navigator, spostato il recupero dei giochi dentro la classe LashMotor

---
+++ roboChechersNXJ/trunk/src/it/polito/Navigation/MathNavigator.java
@@ -0,0 +1,105 @@
+package it.polito.Navigation;
+
+import it.polito.Checkers.Square;
+import lejos.nxt.Motor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TachoMotorPort;
+import lejos.nxt.addon.ColorSensor;
+
+public class MathNavigator implements CheckersNavigator {
+	private static final int lashA = 90, lashB = 230;
+	private static CheckersNavigator navigator = null;
+	private static final LashMotor MA = new LashMotor(MotorPort.A,lashA);
+	private static final LashMotor MB = new LashMotor(MotorPort.B,lashB);
+	private static final ColorSensor CS = new ColorSensor(SensorPort.S1);
+	
+	private boolean calibrated;
+	private int x,y;
+	
+	public static CheckersNavigator getInstance(){
+		if (navigator == null)
+			navigator = new MathNavigator();
+		return navigator;
+	}
+
+
+	public MathNavigator() {
+		this.calibrated = false;
+	}
+
+	public void calibrate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void goHome() throws notCalibratedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void goTo(Square dest) throws notCalibratedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void goTo(int newX, int newY) throws notCalibratedException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isCalibrated() {
+		return calibrated;
+	}
+
+	public boolean isMoving() {
+		return MA.isMoving() || MB.isMoving();
+	}
+
+	public void setSpeed(int speedA, int speedB) {
+		MA.setSpeed(speedA);
+		MB.setSpeed(speedB);
+	}
+
+	public Motor getMotorA() {
+		return MA;
+	}
+
+	public Motor getMotorB() {
+		return MB;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	private void rotateTo(int destAngleA, int destAngleB, boolean nonBlocking) {
+		MA.rotateTo(destAngleA, true);
+		MB.rotateTo(destAngleB, true);
+		if (!nonBlocking)
+			waitForMotors(new Motor[] {MA,MB});
+	}
+
+	private void left() { MB.forward();	}
+	private void right() { MB.backward(); }
+	private void forward() { MA.forward(); }
+	private void backward() { MA.backward(); }
+	
+	/***
+	 * Wait for motor(s) in motorList to stop
+	 * @throws InterruptedException 
+	 */
+	private void waitForMotors(Motor[] motorList){
+		for (int i=0; i < motorList.length; i++){
+			while (motorList[i].isMoving()){
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) { }
+			}
+		}
+	}
+}
