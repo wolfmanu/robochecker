0e943ad461c1d0bd82005b414b995f26768b0ac1
luca <luca@5050fb14-fe7b-11dd-ada4-5153b8187bf2>
2009-01-13 13:54:27 +0000

    Test di Threading funzionante. Parent: 'println' ogni 1 s Child: 'println' ogni 3 s

---
+++ roboChechersNXJ/trunk/src/it/polito/testthread/MainThread.java
@@ -0,0 +1,36 @@
+package it.polito.testthread;
+
+public class MainThread {
+
+	static Thread t;
+	
+	public static void main(String[] args) {
+		childCode();
+		for(int i = 0; i < 20; i++){
+			try{
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				
+			}
+			System.out.println("Parent " + i);
+		}
+	}
+
+	protected synchronized static boolean childCode() {
+    	t = new Thread() {
+    		public void run() {
+    			for(int i = 0; i < 20; i++){
+    				try{
+    					Thread.sleep(3000);
+    				} catch (InterruptedException e) {
+    					
+    				}
+    				System.out.println("Child " + i);
+    			}
+    		}
+        };
+    	t.start();
+        return true;
+    }
+	
+}
